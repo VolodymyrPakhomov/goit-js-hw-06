07b82accad4db037567dfbaf07cf097ed08934f0
VolodymyrPakhomov <vova.zest997@gmail.com>
2022-09-18 15:22:22 +0300

    ч
    ч

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,34 @@
-const form = document.querySelector('.login-form ');
+// const form = document.querySelector('.login-form ');
 
-form.addEventListener('submit', (event)=> {
+// form.addEventListener('submit', (event)=> {
+// event.preventDefault();
+// const{
+//     elements: {email,password}
+// } = event.currentTarget;
+
+// if (email.value === '' || password.value === '') {
+//     alert('Всі поля повинні бути заповнені');
+// }
+// const objktImp {
+//     Email: email.value,
+//     Password: password.value};
+//     console.log(objktImp)
+//     form.reset();
+// });
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
 event.preventDefault();
-const{
-    elements: {email,password}
-} = event.currentTarget;
 
-if (email.value === '' || password.value === '') {
-    alert('Всі поля повинні бути заповнені');
+   const { elements } = event.currentTarget;
+   const { email, password } = elements;
+
+if (!email.value || !password.value) {
+   alert('Все поля должны быть заполнены!');
+   return;
 }
-const objktImp {
-    Email: email.value,
-    Password: password.value};
-    console.log(objktImp)
-    form.reset();
+   
+   console.log({ email: email.value, password: password.value});
+   form.reset();
 });
